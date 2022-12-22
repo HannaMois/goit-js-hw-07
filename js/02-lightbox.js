8a2e97505ff3983d05ef6d6e3845d41bdd37fa01
HannaMois <hannamois135@gmail.com>
2022-12-22 12:20:44 +0200

    Create 02-lightbox.js

---
+++ js/02-lightbox.js
@@ -0,0 +1,24 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const div = document.querySelector(".gallery");
+
+const imageCard = galleryItems.map((obj) => 
+` <div class="gallery__item">
+        <a class="gallery__link" href="${obj.original}">
+         <img
+         class="gallery__image"
+         src="${obj.preview}"
+         alt="${obj.description}"/>
+        </a>
+    </div>`
+    ).join("");
+
+div.insertAdjacentHTML("beforeend", imageCard);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
